d834a7c87b5a14f922490ef28133c27d51d01893
shiva1916 <126543573+shiva1916@users.noreply.github.com>
2025-05-22 13:13:17 +0530

    Update Stock-Price-Prediction.py

--- Stock -Price-Prediction.py
+++ Stock -Price-Prediction.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
 
 
 import yfinance as yf
@@ -13,17 +12,11 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 
 
-# In[4]:
-
-
 # Download data from Yahoo Finance
 stock = 'AAPL'
 data = yf.download(stock, start='2015-01-01', end='2024-12-31')
 data = data[['Close']]
 data.head()
-
-
-# In[6]:
 
 
 # Normalize the data
@@ -42,15 +35,10 @@ X, y = np.array(X), np.array(y)
 X = np.reshape(X, (X.shape[0], X.shape[1], 1))
 
 
-# In[8]:
-
 
 split = int(len(X) * 0.8)
 X_train, X_test = X[:split], X[split:]
 y_train, y_test = y[:split], y[split:]
-
-
-# In[10]:
 
 
 model = Sequential()
@@ -64,22 +52,15 @@ model.compile(optimizer='adam', loss='mean_squared_error')
 model.summary()
 
 
-# In[12]:
-
 
 model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
 
-
-# In[14]:
 
 
 predicted_prices = model.predict(X_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
 
 actual_prices = scaler.inverse_transform(y_test.reshape(-1, 1))
-
-
-# In[16]:
 
 
 plt.figure(figsize=(14,6))
@@ -91,8 +72,6 @@ plt.ylabel('Stock Price')
 plt.legend()
 plt.show()
 
-
-# In[18]:
 
 
 from sklearn.metrics import mean_squared_error
